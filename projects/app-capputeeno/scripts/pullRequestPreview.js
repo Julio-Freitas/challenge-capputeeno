d027809aba14cebb7a30c6ff6a7a50c01cafa499
@julin.freitas <julio.freitas@solides.com.br>
2023-07-21 11:04:26 -0300

    feat: custom deploy preview

--- projects/app-capputeeno/scripts/pullRequestPreview.js
+++ projects/app-capputeeno/scripts/pullRequestPreview.js
@@ -1,1 +1,9 @@
-console.log('ola mundo,...');
+import { execSync } from 'child_process';
+
+console.log('[DEPLOY_PREVIEW]: START');
+
+const command = 'yarn deploy:staging';
+const output = execSync(command, { encoding: 'utf-8' });
+console.log(output);
+
+console.log('[DEPLOY_PREVIEW]: END');
