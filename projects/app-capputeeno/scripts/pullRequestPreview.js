3444feb49aceeaf430d7c434a6151bc3a1fdaccc
Julio Cesar <julio.clf31@gmail.com>
2023-07-21 11:36:02 -0300

    feat: new scripts in ci/cd (#11)
    * feat: new scripts in ci/cd
    
    * feat: new scripts in ci/cd fix name script
    
    * feat: new scripts in ci/cd fix name script
    
    * feat: custom deploy preview
    
    * fix: custom deploy preview
    
    * fix: custom deploy preview adjusment params
    
    * feat: update msg deploy preview
    
    ---------
    
    Co-authored-by: @julin.freitas <julio.freitas@solides.com.br>

--- projects/app-capputeeno/scripts/pullRequestPreview.js
+++ projects/app-capputeeno/scripts/pullRequestPreview.js
@@ -1,1 +1,12 @@
-console.log('ola mundo,...');
+import { execSync } from 'child_process';
+
+console.log('=== [DEPLOY_PREVIEW]: START ===');
+
+const command = 'yarn deploy:staging';
+const output = execSync(command, { encoding: 'utf-8' });
+const outputLines = output.split('\n');
+const DEPLOY_URL_PREVIEW = outputLines[outputLines.length - 1];
+
+console.log(`Preview_URL: ${DEPLOY_URL_PREVIEW}`);
+
+console.log('=== [DEPLOY_PREVIEW]: END ===');
